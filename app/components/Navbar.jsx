a30fedfa22d546060e66c1a31c5f803f3e60b167
tedeveloper1 <tuyisengee09@gmail.com>
2025-04-23 13:42:08 +0200

    Navigation

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <h1>StudyGenius</h1>
+        <div className='flex '>
+            <div><h2>About</h2></div>
+            <div><h2>Log in</h2></div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
